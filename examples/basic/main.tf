5d8a4c4e3d06d1792812894a2dbb54356cd5637a
Martin Dojcak <martin.dojcak@lablabs.io>
2021-08-30 14:23:20 +0200

    Update example

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -2,7 +2,7 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.6.0"
 
-  name               = "metrics-server-vpc"
+  name               = "node-exporter-vpc"
   cidr               = "10.0.0.0/16"
   azs                = ["eu-central-1a", "eu-central-1b"]
   public_subnets     = ["10.0.101.0/24", "10.0.102.0/24"]
@@ -16,14 +16,14 @@ module "eks_cluster" {
   region     = "eu-central-1"
   subnet_ids = module.vpc.public_subnets
   vpc_id     = module.vpc.vpc_id
-  name       = "metrics-server"
+  name       = "node-exporter"
 }
 
 module "eks_node_group" {
   source  = "cloudposse/eks-node-group/aws"
   version = "0.25.0"
 
-  cluster_name   = "metrics-server"
+  cluster_name   = "node-exporter"
   instance_types = ["t3.medium"]
   subnet_ids     = module.vpc.public_subnets
   min_size       = 1
