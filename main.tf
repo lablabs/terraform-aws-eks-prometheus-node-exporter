9d6a572b5096ea6e6bb9522e89804025a0308506
Attila Sovak <attila.sovak@gmail.com>
2025-05-23 09:43:15 +0200

    Migrate to universal addon (prometheus-node-exporter) (#8)
    # Description
    Migrate AWS EKS Grafana addon to Universal Addon format
    
    Keeping repo as bitnami for now, due to missing operator in prometheus
    charts.
    
    ### Does this PR introduce a breaking change?
    **No**
    
    ## Type of change
    
    - [ ] A bug fix (PR prefix `fix`)
    - [ ] A new feature (PR prefix `feat`)
    - [X] A code change that neither fixes a bug nor adds a feature (PR
    prefix `refactor`)
    - [ ] Adding missing tests or correcting existing tests (PR prefix
    `test`)
    - [ ] Changes that do not affect the meaning of the code like
    white-spaces, formatting, missing semi-colons, etc. (PR prefix `style`)
    - [ ] Changes to our CI configuration files and scripts (PR prefix `ci`)
    - [ ] Documentation only changes (PR prefix `docs`)
    
    ## How Has This Been Tested?
    In our lab it was loaded and tested
    
    Related to: https://github.com/lablabs/tf-infra/issues/998

--- main.tf
+++ main.tf
@@ -1,21 +1,23 @@
-resource "helm_release" "prometheus_node_exporter" {
-  count            = var.enabled && !var.argo_application_enabled ? 1 : 0
-  chart            = var.helm_chart_name
-  create_namespace = var.helm_create_namespace
-  namespace        = var.k8s_namespace
-  name             = var.helm_release_name
-  version          = var.helm_chart_version
-  repository       = var.helm_repo_url
+/**
+ * # AWS EKS Prometheus node exporter Terraform module
+ *
+ * A terraform module to deploy [Prometheus node exporter](https://prometheus.io/docs/guides/node-exporter/) on Amazon EKS cluster.
+ *
+ * ![Terraform validation](https://github.com/lablabs/terraform-aws-eks-prometheus-node-exporter/workflows/Terraform%20validation/badge.svg?branch=master)
+ * [![pre-commit](https://img.shields.io/badge/pre--commit-enabled-success?logo=pre-commit&logoColor=white)](https://github.com/pre-commit/pre-commit)
+ */
 
-  values = [
-    var.values
-  ]
+locals {
 
-  dynamic "set" {
-    for_each = var.settings
-    content {
-      name  = set.key
-      value = set.value
-    }
+  addon = {
+    name      = "prometheus-node-exporter"
+    namespace = "kube-system"
+
+    helm_chart_version = "4.43.1"
+    helm_repo_url      = "https://prometheus-community.github.io/helm-charts"
   }
+
+  addon_values = yamlencode({})
+
+  addon_depends_on = []
 }
